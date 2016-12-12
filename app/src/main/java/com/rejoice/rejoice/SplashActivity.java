c051ce9d444bf34caa55f0f662d890d9a56bb466
vikeshjhajhria <vikesh.jhajhria@gmail.com>
2016-12-12 16:51:44 +0530

    12-12-16

--- app/src/main/java/com/rejoice/rejoice/SplashActivity.java
+++ app/src/main/java/com/rejoice/rejoice/SplashActivity.java
@@ -2,9 +2,17 @@ package com.rejoice.rejoice;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class SplashActivity extends AppCompatActivity {
@@ -25,4 +33,5 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, SPLASH_TIME_OUT);
     }
+
 }
